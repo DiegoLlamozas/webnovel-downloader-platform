5f377d7172ac4affee5a39d76da6199a2ab69415
DiegoLlamozas <diegollamozas911@gmail.com>
2026-02-19 22:06:18 -0500

    chore: initial setup.

---
+++ WebNovelManager.WebNovelDownloaderPlatform.API/Shared/Application/Internal/EventHandlers/IEventHandler.cs
@@ -0,0 +1,10 @@
+﻿using WebNovelManager.WebNovelDownloaderPlatform.API.Shared.Domain.Model.Events;
+
+using Cortex.Mediator.Notifications;
+
+namespace WebNovelManager.WebNovelDownloaderPlatform.API.Shared.Application.Internal.EventHandlers;
+
+public interface IEventHandler<in TEvent> : INotificationHandler<TEvent> where TEvent : IEvent
+{
+    
+}
